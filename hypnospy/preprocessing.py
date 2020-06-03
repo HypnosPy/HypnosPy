9568c85de9397ae73fadbbdaebb5ad2673a2d07f
Ignacio <ignacio_perez_pozuelo@alumni.brown.edu>
2020-06-03 18:07:38 +0100

    added a bit more of skeleton

--- hypnospy/preprocessing.py
+++ hypnospy/preprocessing.py
@@ -18,6 +18,70 @@ class PreProcessing(object):
         self.additional_data = additional_data
 
 
+    
+    def get_wearable_type(self):
+        """ Obtain device type 
+        Used to decide which way to parse the data from input file
+        
+        
+        :return: Wearable type (Axivity, GeneActiv, Actigraph, Actiwatch and Apple Watch currently supported)
+        """"
+        if file.lower().endsiwth('.cwa') or file.lower().endswith('.cwa.gz') or file.lower().endswith('CWA'):
+            return process_axivity(file)
+        elif file.lower().endswith('.bin'):
+            return process_geneactiv(file)
+        elif file.lower().endswith('.dat'):
+            return process_actigraph(file)
+        elif file.lower().endswith('.csv'):
+            return 
+        # here ask for which device: ={ "applewatch":"Apple Watch","apple" [...] "actiwatch"[...]
+        # ask if the data is raw or not--> if not--> move down the pecking order
+        elif file.lower().endswith('.dta'):
+            return 
+        # here ask for which device: ={ "applewatch":"Apple Watch","apple" [...] "actiwatch"[...]
+        # ask if the data is raw or not--> if not--> move down the pecking order
+        else:
+        print("ERROR: Wearable format not supported for file: " + file)
+        
+    
+    def process_axivity(cwaFile):
+        """ Use PAMPRO here
+        """
+        pass
+    
+    def process_actigraph(datFile):
+        """ Use PAMPRO here
+        """
+        pass
+   
+    def process_geneactiv(datFile):
+        """ Use PAMPRO here
+        """
+        pass
+    
+    def run_nonwear()
+        """ Use PAMPRO autocalibration
+        """
+        pass
+    def calibrate_data()
+        """ Use PAMPRO autocalibration
+        """
+        pass
+    
+    def obtain_PA_metrics()
+        """" Use PAMPRO for derivation of VM, VMHPF, ENMO, Pitch, Roll, Yaw
+              Include proxy for MVPA, PA
+        """"
+        # run this if class isn't count based 
+        # returns something that can be then summarized
+        pass
+    
+    def obtain_data():
+        """ Here we will define epochperiod, columns to be included, and generate
+        the new .hpy file
+        """
+        pass
+    
     def export_hypnospy(self, filename):
         pass
 
