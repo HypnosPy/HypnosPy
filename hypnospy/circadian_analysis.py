6cac4e618d479dbfe8dc5d66e31dfe4bd7f83432
Ignacio <ignacio_perez_pozuelo@alumni.brown.edu>
2020-06-03 17:10:11 +0100

    skeleton circadiananalysis.py

---
+++ hypnospy/circadian_analysis.py
@@ -0,0 +1,18 @@
+from hypnospy import Wearable
+
+class CircadianAnalysis(object):
+
+    def __init__(self, wearable: Wearable):
+        self.wearable = wearable
+
+    def run_SSA():
+        # apply SSA analysis
+        pass
+    
+    def run_cosinor():
+        # apply cosinor method
+        pass
+        
+    def run_PSD():
+        #apply power spectral density analysis
+        pass
