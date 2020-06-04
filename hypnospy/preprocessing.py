c5942aaff9ea1ad5208c5340ca097bdc75bc89c9
Ignacio <ignacio_perez_pozuelo@alumni.brown.edu>
2020-06-04 22:38:58 +0100

    Update preprocessing.py

--- hypnospy/preprocessing.py
+++ hypnospy/preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-class PreProcessing(object):
+class RawProcessing(object):
 
     def __init__(self, filename=None, device_location=None, additional_data=None):
         self.possible_locations = ["bw", "hip", "dw", "ndw", "chest", "hp_ch", "hp_bw", "all"]
@@ -59,11 +59,11 @@ class PreProcessing(object):
         """
         pass
     
-    def run_nonwear()
+    def run_nonwear():
         """ Use PAMPRO autocalibration
         """
         pass
-    def calibrate_data()
+    def calibrate_data():
         """ Use PAMPRO autocalibration
         """
         pass
@@ -75,7 +75,51 @@ class PreProcessing(object):
         # run this if class isn't count based 
         # returns something that can be then summarized
         pass
+  
+  class dataReader():
+        r""" Reads processed raw files 
+        and files obtained that are already proceessed"
+        
+        Params
+        ---------
+        
+        """
+        
+        def __init__(self, wearable_type, datareader=[]):
+            
+            #store wearable type
+            self.__wearable_type = wearable_type
+            
+            # store list of data reader
+            self.__datareader = datareader
+        
+        @property
+        def wearable_type(self):
+            r""" This function will take in the type of reader"""
+            #1. Check file type from above processing
+            
+            #2,. 
+        
+        return 
+        
+        @property
+        def datareader(self):
+            r""" This function will """
+            #1. Check file type 
+            
+            #2. Determine  
+        
+        return 
     
+    def find_sleepwin():
+        # Apply our method based on HR here
+        
+        # if no HR look for expert annotations _anno
+        
+        # if no annotations look for diaries
+        
+        # if no diaries apply Van Hees heuristic method
+
     def obtain_data():
         """ Here we will define epochperiod, columns to be included, and generate
         the new .hpy file
