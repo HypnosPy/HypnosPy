34bf36de6513e441a711c72302b556ef0bf6d5f4
Ignacio <ignacio_perez_pozuelo@alumni.brown.edu>
2020-06-05 11:49:35 +0100

    Update time_series_processing.py

--- hypnospy/time_series_processing.py
+++ hypnospy/time_series_processing.py
@@ -4,7 +4,38 @@ class TimeSeriesProcessing(object):
 
     def __init__(self):
         pass
+    def data_load(self):
+        """ Here we need to load the data and determine:
+        potentially by fetching from class wearable
+        (1) what type of file is it
+        (2) is it multimodal
+        (3) length/type- night only/ full
+        (4) sampling rate
+        """
+    pass
 
+    def featurize(self):
+        """ Uses tsfresh to extract time series features
+        """
+        pass
+        
+    def day_night_split(self):
+       """ Splits day night into 9AM to 9PM chunks (day) and 9PM to 9AM (night) (saves flags as columns)
+        
+       """
+       #load data and process timestamps
+        if file.lower().endsiwth('.csv'):
+            df = pd.read_csv(@params)
+        elif file.lower().endsiwth('.dta'):
+            df = pd.read_stata(@params)
+        else:
+        print("ERROR: Wearable format not supported for file: " + file)
+        
+        # do time series and formatting transformations, etc.
+        
+        pass
+        
+      
     def detect_sleep_boundaries(self, strategy):
         """
             Detected the sleep boundaries.
@@ -14,12 +45,20 @@ class TimeSeriesProcessing(object):
 
             strategy: "hr"
         """
-        # if has hr -> use hr, if has annotation -> use annotations,
-        pass
+    
+        # Include HR sleeping window approach here (SLEEP 2020 paper)
+        
+        # if no HR look for expert annotations _anno
+        
+        # if no annotations look for diaries
+        
+        # if no diaries apply Crespo (periods of innactivity)
+        
+        # if no diaries apply Van Hees heuristic method
+        # this method requires triaxial accelerometry to be used
+    pass
 
-    def score_sleep(method, sampling):
-        # TODO: include all heuristics, 1-2 pretrained LSTM
-        pass
+
 
 
 
