2eb9be4a496e2c470de2849e2d5b1707c4ea7e0f
Marius Posa <mariusposa10@gmail.com>
2020-06-24 23:45:40 +0100

    add get_windows, fix nonlinear analysis

--- Notebooks/nonlinear_analysis.py
+++ Notebooks/nonlinear_analysis.py
@@ -11,29 +11,36 @@ from datetime import datetime, date, time, timedelta
 from os import path
 from collections import defaultdict
 import nolds
-
     
 def get_nonlin_params(df,col):
     params = defaultdict(dict)
-    params['hurst'] = nolds.hurst_rs(df[col],debug_data=False)
+    #params['hurst'] = nolds.hurst_rs(df[col],debug_data=False)
     params['DFA'] = nolds.dfa(df[col],debug_data=False)
     params['sampen'] = nolds.sampen(df[col],debug_data=False)
     #params['lyap1'] = nolds.lyap_r(df[col],debug_data=False)
     return params
 
-def get_nonlinear(self,cols = ['ENMO','mean_hr','hrv_ms'],all_data=True, sleep_wise=True,wake_wise=True):
+def get_nonlinear(self,cols = ['ENMO','mean_hr','hrv_ms']):
+    
     nonlin = defaultdict(dict)
-    df_1 = self.data
-    #df_1['sleep_window'] = df_1['sleep_window'].fillna(method='ffill')
-    df_1['sleep_window_0.4'] = df_1['sleep_window_0.4'].fillna(method='bfill')
+    
     for col in cols:
-        if sleep_wise==True:
-           sleep = df_1[df_1['sleep_window_0.4']==2]
-           nonlin[col]['sleep'] = get_nonlin_params(sleep,col)
-        if wake_wise==True:
-            wake = df_1[df_1['sleep_window_0.4']==1]
-            nonlin[col]['wake'] = get_nonlin_params(wake,col)
-        if all_data==True:
-            nonlin[col]['all'] = get_nonlin_params(df_1,col)
+        column = defaultdict(dict)
+        column_sleep = defaultdict(dict)
+        column_wake = defaultdict(dict)
+        for idx in range(len(self.sleep_windows)):
+            params = defaultdict(dict)
+            params['DFA'] = nolds.dfa(self.sleep_windows[idx][col],debug_data=False)
+            params['SampEn'] = nolds.sampen(self.sleep_windows[idx][col],debug_data=False)
+            column_sleep[idx] = params
+        for idx in range(len(self.wake_windows)):
+            params = defaultdict(dict)
+            params['DFA'] = nolds.dfa(self.wake_windows[idx][col],debug_data=False)
+            params['SampEn'] = nolds.sampen(self.wake_windows[idx][col],debug_data=False)
+            column_wake[idx] = params
+        column['sleep'] = column_sleep
+        column['wake'] = column_wake
+        
+        nonlin[col] = column
     self.nonlinear = nonlin
     return self
