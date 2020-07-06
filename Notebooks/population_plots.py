5d88a107a3c2dd8e8272a0049b4027d9715c23d5
Marius Posa <mariusposa10@gmail.com>
2020-07-06 13:52:42 +0100

    notebook for hr algo optimisation + func updates

--- Notebooks/population_plots.py
+++ Notebooks/population_plots.py
@@ -282,7 +282,7 @@ def plot_hr(d):
     #plt.tight_layout()
     return fig
 
-def plot_algos(d,i,diary):
+def plot_algos(d,i,diary,qtl,length):
     fig, ax1 = plt.subplots(4, 1, figsize= (20, 10),sharex=True)
     #ax1.set_title("Physical activity and sedentary time per hour")                        
     #Resampling: hourly
@@ -299,8 +299,8 @@ def plot_algos(d,i,diary):
     #Add grey windows for sleep
     for idx in range(len(diary)):
         ax1[0].axvspan(diary['sleep_onset'][idx],diary['sleep_offset'][idx],facecolor='salmon',alpha=1) 
-    for idx in range(len(d[i].sleep_rec[0.4])):
-        ax1[0].axvspan(d[i].sleep_rec[0.4]['sleep_onset'][idx],d[i].sleep_rec[0.4]['sleep_offset'][idx],facecolor='black',alpha=0.5) 
+    for idx in range(len(d[i].sleep_rec[qtl][length])):
+        ax1[0].axvspan(d[i].sleep_rec[qtl][length]['sleep_onset'][idx],d[i].sleep_rec[qtl][length]['sleep_offset'][idx],facecolor='black',alpha=0.5) 
  
     
     ax1[1].plot(df2_h.index,df2_h['pitch_mean_dw'].abs(), label='pitch_dw',linewidth=0.4, color ='green',alpha=0.3)
@@ -345,3 +345,56 @@ def plot_algos(d,i,diary):
     #fig.legend()
     plt.tight_layout()
     return fig 
+
+
+def plot_daily_vh(d,i):
+    import matplotlib.dates as dates
+    DFList = [pd.DataFrame(group[1]) for group in d[i].data.groupby(d[i].data.index.day)]
+    fig, ax1 = plt.subplots(len(DFList), 1, figsize= (14, 20))
+    #ax1.set_title("Physical activity and sedentary time per hour")                        
+    for idx in range(len(DFList)):
+        #Resampling: hourly
+        df2_h = DFList[idx]
+        ax1[idx].tick_params(axis='x', which='both',bottom=False,top=False, labelbottom=True, rotation=0)
+        ax1[idx].set_facecolor('snow')
+        #ax1[idx].set_xlabel('Time')
+        ax1[idx].set_ylim(0,max(d[i].data['ENMO']/10))
+        ax1[idx].set_xlim(DFList[idx].index[0] - timedelta(hours=DFList[idx].index[0].hour) - 
+                          timedelta(minutes=DFList[idx].index[0].minute) - timedelta(seconds=DFList[idx].index[0].second), 
+                          DFList[idx].index[0] - timedelta(hours=DFList[idx].index[0].hour) - 
+                          timedelta(minutes=DFList[idx].index[0].minute) - timedelta(seconds=DFList[idx].index[0].second) +
+                          timedelta(minutes=1439))
+
+        ax1[idx].plot(df2_h.index, df2_h['ENMO']/10,label='ENMO', linewidth=1, color ='black',alpha=1)
+        ax1[idx].fill_between(df2_h.index, 0, df2_h['min_VigPA']*200,facecolor ='darkgreen',alpha=0.7, label='VPA',edgecolor='darkgreen')
+        ax1[idx].fill_between(df2_h.index, 0, df2_h['min_MVPA']*200,facecolor ='forestgreen',alpha=0.7, label='MVPA',edgecolor='forestgreen')
+        
+        ax1[idx].fill_between(df2_h.index, 0, df2_h['min_LPA']*200,facecolor ='lightgreen',alpha=0.7, label='LPA',edgecolor='lightgreen')
+        ax1[idx].fill_between(df2_h.index, 0, (df2_h['MET_Sed'])*200,facecolor ='palegoldenrod',alpha=0.6,label='sedentary',edgecolor='palegoldenrod')
+        ax1[idx].fill_between(df2_h.index, 0, -(df2_h['sleep_window_0.4']-2)*200,facecolor ='royalblue',alpha=1,label='sleep')
+        ax1[idx].fill_between(df2_h.index, 0, (df2_h['wake_window_0.4'])*200,facecolor ='cyan',alpha=1,label='wake')
+        
+        ax1[idx].fill_between(df2_h.index, 0, (df2_h['sleep_window_'+'dw'])*50,facecolor ='black',alpha=1,label='vanhees_slp_dw')
+        #ax1[idx].fill_between(df2_h.index, 0, (df2_h['sleep_window_'+'roll_dw'])*25,facecolor ='grey',alpha=1,label='vanhees_roll_slp')
+        #ax1[idx].fill_between(df2_h.index, 0, (1-df2_h['crespo'])*10,facecolor ='cyan',alpha=0.1,label='crespo_rest')
+        
+        ax1[idx].set_xticks([])
+        ax1[idx].set_yticks([])
+    #    for ax in ax_row:
+        # create a twin of the axis that shares the x-axis
+        ax2 = ax1[idx].twinx()
+        #ax2.set_ylabel('mean_HR')  # we already handled the x-label with ax1
+        ax2.plot(df2_h.index, df2_h['mean_hr'],label='HR', color='red')
+        ax2.set_ylim(30,max(d[i].data['mean_hr']))
+        ax2.set_xticks([])
+        ax2.set_yticks([])
+              
+    ax1[-1].xaxis.set_minor_locator(dates.HourLocator(interval=4))   # every 4 hours
+    ax1[-1].xaxis.set_minor_formatter(dates.DateFormatter('%H:%M')) # hours and minutes
+    #ax1[-1].xaxis.set_major_locator(dates.DayLocator(interval=2))    # every day
+    #ax1[-1].xaxis.set_major_formatter(dates.DateFormatter('%d-%m-%Y'))
+
+    handles, labels = ax1[-1].get_legend_handles_labels()
+    handles2, labels2 = ax2.get_legend_handles_labels()
+    fig.legend(handles+handles2, labels+labels2, loc='lower center',ncol=4)
+    return fig
