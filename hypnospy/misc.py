4d3e33840754508e266a74b6915afbcde1aad95e
joaopalotti@gmail.com <joaopalotti@gmail.com>
2020-08-01 02:13:49 +0300

    Changed the implementation of find_largest_sleep to improve execution time.

--- hypnospy/misc.py
+++ hypnospy/misc.py
@@ -1,3 +1,6 @@
+import warnings
+
+
 def get_consecutive_serie(df_in, col):
     df = df_in.copy()
 
@@ -6,3 +9,22 @@ def get_consecutive_serie(df_in, col):
     df["_lenght"] = df[["_lenght", "_seq_id"]].groupby("_seq_id").transform("count")
 
     return df["_lenght"], df["_seq_id"]
+
+
+def find_largest_sequence(df_orig, candidate, output_col, seq_length_col="hyp_seq_length", seq_id_col="hyp_seq_id"):
+    df = df_orig.copy()
+    df[output_col] = False
+
+    df_candidates = df[df[candidate] == True]
+
+    if df_candidates.empty:
+        warnings.warn("Day has no sleep period!")
+        df[output_col] = -1
+        return df[output_col]
+
+    # Mark the largest period as "sleep_period"
+    largest_seqid = df_candidates.iloc[df_candidates[seq_length_col].argmax()][seq_id_col]
+    largest = df_candidates[df_candidates[seq_id_col] == largest_seqid]
+    df.loc[largest.index, output_col] = True
+
+    return df[output_col]
