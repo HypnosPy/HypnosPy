b14fb1f3ffe9ddbedb87d212b8dd9f6e32bdb28e
joaopalotti@gmail.com <joaopalotti@gmail.com>
2020-09-10 22:55:32 -0300

    Several viz improvements

--- hypnospy/analysis/visualization.py
+++ hypnospy/analysis/visualization.py
@@ -21,8 +21,8 @@ class Viewer(object):
 
 
     def view_signals(self, signal_categories: list = ["activity", "hr", "pa_intensity", "sleep"],
-                     other_signals: list = [], signal_as_area: list = [], frequency: str = "60S", sleep_cols: str = None,
-                     select_days: list = None, colors: list = [], alpha: float = 1.0,
+                     other_signals: list = [], signal_as_area: list = [], frequency: str = "60S",
+                     sleep_cols: list = [], select_days: list = None, colors: list = [], alpha: float = 1.0,
                      zoom: tuple = ("00:00:00", "23:59:59")
                      ):
 
@@ -32,8 +32,30 @@ class Viewer(object):
 
 
     @staticmethod
+    def get_day_label(df):
+
+        s = ""
+        startdate = df.index[0]
+        enddate = df.index[-1]
+
+        if startdate.day == enddate.day:
+            s = "%d - %s\n %s" % (startdate.day, calendar.month_name[startdate.month][:3], calendar.day_name[startdate.dayofweek])
+
+        else:
+            if startdate.month == enddate.month:
+                s = "%d/%d - %s\n %s/%s" % (
+                startdate.day, enddate.day, calendar.month_name[startdate.month][:3], calendar.day_name[startdate.dayofweek][:3], calendar.day_name[enddate.dayofweek][:3])
+            else:
+                s = "%d - %s/%d - %s\n %s/%s" % (
+                    startdate.day, calendar.month_name[startdate.month][:3], enddate.day, calendar.month_name[enddate.month][:3],
+                    calendar.day_name[startdate.dayofweek][:3], calendar.day_name[enddate.dayofweek][:3])
+
+        return s
+
+
+    @staticmethod
     def view_signals_wearable(wearable: Wearable, signal_categories: list, other_signals: list, signal_as_area: list,
-                              frequency: str, sleep_cols: str, select_days: list, colors: list, alpha: float,
+                              frequency: str, sleep_cols: list, select_days: list, colors: list, alpha: float,
                               zoom: tuple):
 
         sns.set_context("talk", font_scale=1.3, rc={"axes.linewidth": 2, 'image.cmap': 'plasma', })
@@ -75,11 +97,14 @@ class Viewer(object):
             raise ValueError("Aborting: Empty list of signals to show.")
 
         cols.append(wearable.time_col)
-        cols.append(wearable.experiment_day_col)
         for col in set(other_signals + signal_as_area):
             cols.append(col)
 
         df_plot = wearable.data[cols].set_index(wearable.time_col).resample(frequency).sum()
+
+        # Add column for experiment day. It wil lbe resampled using the the mean
+        cols.append(wearable.experiment_day_col)
+        df_plot[wearable.experiment_day_col] = wearable.data[[wearable.time_col, wearable.experiment_day_col]].set_index(wearable.time_col).resample(frequency).median()
 
         if zoom is not None:
             df_plot = df_plot.between_time(zoom[0], zoom[1], include_start=True, include_end=True)
@@ -108,25 +133,6 @@ class Viewer(object):
             maxy = 2
 
             df2_h = dfs_per_day[idx]
-            ax1[idx].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=True, rotation=0)
-            ax1[idx].set_facecolor('snow')
-
-            if zoom[0] == "00:00:00" and zoom[1] == "23:59:59": # Default options, we use hour_start_experiment
-                new_start_datetime = dfs_per_day[idx].index[0] - timedelta(hours=dfs_per_day[idx].index[0].hour - wearable.hour_start_experiment,
-                                                      minutes=dfs_per_day[idx].index[0].minute, seconds=dfs_per_day[idx].index[0].second),
-                new_end_datetime = dfs_per_day[idx].index[0] - timedelta(hours=dfs_per_day[idx].index[0].hour - wearable.hour_start_experiment,
-                                                      minutes=dfs_per_day[idx].index[0].minute, seconds=dfs_per_day[idx].index[0].second) + timedelta(minutes=1439)
-
-            else:
-                new_start_date = dfs_per_day[idx].index[0].date()
-                new_start_datetime = datetime(new_start_date.year, new_start_date.month, new_start_date.day, int(zoom[0][0:2]), int(zoom[0][3:5]), int(zoom[0][6:8]))
-                new_start_datetime = pd.to_datetime(new_start_datetime)
-
-                new_end_date = dfs_per_day[idx].index[-1].date()
-                new_end_datetime = datetime(new_end_date.year, new_end_date.month, new_end_date.day, int(zoom[1][0:2]), int(zoom[1][3:5]), int(zoom[1][6:8]))
-                new_end_datetime = pd.to_datetime(new_end_datetime)
-
-            ax1[idx].set_xlim(new_start_datetime, new_end_datetime)
 
             if "activity" in signal_categories:
                 maxy = max(maxy, df2_h[wearable.get_activity_col()].max())
@@ -149,32 +155,27 @@ class Viewer(object):
             if "sleep" in signal_categories:
                 facecolors = ['royalblue', 'green', 'orange']
                 # maxy = 10000
-                maxy = 100
                 endy = 0
-                addition = (maxy / len(facecolors))
+                addition = (maxy / len(sleep_cols))
                 for i, sleep_col in enumerate(sleep_cols):
                     starty = endy
                     endy = endy + addition
                     sleeping = df2_h[sleep_col]  # TODO: get a method instead of an attribute
                     ax1[idx].fill_between(df2_h.index, starty, endy, where=sleeping, facecolor=facecolors[i],
-                                          alpha=alpha, label=sleep_col)
-                    # ax1[idx].fill_between(df2_h.index, starty, endy, where=~sleeping, facecolor="gray", alpha=0.7,
-                    #                      label=" ")
-                    # ax1[idx].fill_between(df2_h.index, 0, (df2_h['wake_window_0.4']) * 200, facecolor='cyan', alpha=1,
-                    #                   label='wake')
+                                          alpha=0.7, label=sleep_col)
+
 
             if "diary" in signal_categories and wearable.diary_onset in df2_h.keys() and wearable.diary_offset in df2_h.keys():
                 diary_event = df2_h[(df2_h[wearable.diary_onset] == True) | (df2_h[wearable.diary_offset] == True)].index
-                ax1[idx].vlines(x=diary_event, ymin=0, ymax=100, facecolor='black', alpha=alpha, label='Diary',
+                ax1[idx].vlines(x=diary_event, ymin=0, ymax=maxy, facecolor='black', alpha=alpha, label='Diary',
                                 linestyles="dashed")
 
             for i, col in enumerate(other_signals):
                 #colors = ["orange", "violet", "pink", "gray"] # Change to paramters
                 ax1[idx].plot(df2_h.index, df2_h[col], label=col, linewidth=1, color=colors[i], alpha=alpha)
 
-            #maxy = 1000
             endy = 0
-            addition = (maxy / len(signal_as_area))
+            addition = 0 if len(signal_as_area) == 0 else (maxy / len(signal_as_area))
             for i, col in enumerate(signal_as_area):
                 #colors = ["orange", "violet", "pink", "gray"]
                 starty = endy
@@ -184,15 +185,32 @@ class Viewer(object):
                                       alpha=alpha, label=col)
 
 
+            # configure time limits (y-axis) for plot.
+            ax1[idx].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=True, rotation=0)
+            ax1[idx].set_facecolor('snow')
 
-            ax1[idx].set_ylabel("%d-%s\n%s" % (dfs_per_day[idx].index[-1].day,
-                                               calendar.month_name[dfs_per_day[idx].index[-1].month][:3],
-                                               calendar.day_name[dfs_per_day[idx].index[-1].dayofweek]),
-                                rotation=0, horizontalalignment="right", verticalalignment="center")
+            if zoom[0] == "00:00:00" and zoom[1] == "23:59:59": # Default options, we use hour_start_experiment
+                new_start_datetime = df2_h.index[0] - timedelta(hours=(df2_h.index[0].hour - wearable.hour_start_experiment) % 24,
+                                                      minutes=df2_h.index[0].minute, seconds=df2_h.index[0].second),
+                new_end_datetime = df2_h.index[0] - timedelta(hours=(df2_h.index[0].hour - wearable.hour_start_experiment) % 24,
+                                                      minutes=df2_h.index[0].minute, seconds=df2_h.index[0].second) + timedelta(minutes=1439)
+
+            else:
+                new_start_date = df2_h.index[0].date()
+                new_start_datetime = datetime(new_start_date.year, new_start_date.month, new_start_date.day, int(zoom[0][0:2]), int(zoom[0][3:5]), int(zoom[0][6:8]))
+                new_start_datetime = pd.to_datetime(new_start_datetime)
+
+                new_end_date = df2_h.index[-1].date()
+                new_end_datetime = datetime(new_end_date.year, new_end_date.month, new_end_date.day, int(zoom[1][0:2]), int(zoom[1][3:5]), int(zoom[1][6:8]))
+                new_end_datetime = pd.to_datetime(new_end_datetime)
+
+            ax1[idx].set_xlim(new_start_datetime, new_end_datetime)
+
+            y_label = Viewer.get_day_label(df2_h)
+            ax1[idx].set_ylabel("%s" % y_label, rotation=0, horizontalalignment="right", verticalalignment="center")
 
             ax1[idx].set_xticks([])
             ax1[idx].set_yticks([])
-            # ax1[idx].set_label(["Experiment Day %d" % (idx)])
 
             # create a twin of the axis that shares the x-axis
             if "hr" in signal_categories:
@@ -218,3 +236,33 @@ class Viewer(object):
         fig.legend(handles, labels, loc='lower center', ncol=len(cols), fontsize=14, shadow=True)
         fig.savefig('%s_signals.pdf' % (wearable.get_pid()), dpi=300, transparent=True, bbox_inches='tight')
 
+
+    @staticmethod
+    def plot_one_bland_altman(df_in, a, b, label1, label2, plotname=None):
+        sns.set_context("talk", font_scale=1.3, rc={"axes.linewidth": 2, 'image.cmap': 'plasma', })
+        plt.rc('font', family='serif')
+
+        df = df_in.copy()
+        df["tst_" + a] = df["offset_" + a] - df["onset_" + a]
+        df["tst_" + b] = df["offset_" + b] - df["onset_" + b]
+        df["tst_average"] = (df["tst_" + a] + df["tst_" + b]) / 2.
+        df["tst_diff"] = (df["tst_" + a] - df["tst_" + b])
+
+        df["tst_average"] = df['tst_average'].dt.total_seconds() / 3600
+        df["tst_diff"] = df['tst_diff'].dt.total_seconds() / 3600
+
+        fig = plt.figure(figsize=(14, 8))
+        plt.scatter(df['tst_average'], df['tst_diff'], color='blue')
+        plt.axhline(df['tst_diff'].mean(), color='grey')
+        plt.axhline(df['tst_diff'].mean() + df['tst_diff'].std(), linestyle='dashed', color='grey')
+        plt.axhline(df['tst_diff'].mean() - df['tst_diff'].std(), linestyle='dashed', color='grey')
+        plt.xlabel('TST average %s-%s (hours)' % (label1, label2))
+        plt.ylabel('TST difference %s-%s (hours)' % (label1, label2))
+        plt.title('Bland-Altman plot comparing %s and %s.' % (label1, label2))
+        # plt.show(fig)
+
+        if plotname:
+            plt.savefig(plotname, dpi=200, bbox_inches='tight', facecolor='w', edgecolor='w', orientation='portrait',
+                        transparent=True, pad_inches=0.01)
+
+        return plt
