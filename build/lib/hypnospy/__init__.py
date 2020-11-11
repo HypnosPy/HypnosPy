cb03e503b7fdb1579dca31c23477947651fc4c88
Joao Palotti <joaopalotti@gmail.com>
2020-11-11 17:06:16 +0300

    Removed non-essential files

--- build/lib/hypnospy/__init__.py
+++
@@ -1,6 +0,0 @@
-#__all__ = ["Wearable", "data"]
-
-from .diary import Diary
-from .wearable import Wearable
-from .experiment import  Experiment
-
--- build/lib/hypnospy/data/__init__.py
+++
@@ -1,1 +0,0 @@
-from .preprocessing import RawProcessing, ActiwatchSleepData, MESAPreProcessing, MMASHPreProcessing
