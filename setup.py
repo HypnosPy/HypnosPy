0dd193d5c585dcb4cbc2e044765d35eda7e42c7d
Joao Palotti <joaopalotti@gmail.com>
2020-11-18 11:10:46 +0300

    Version 0.0.3 containing a validator method

--- setup.py
+++ setup.py
@@ -7,19 +7,21 @@ requirements = [
     "numpy >= 1.0.0",
     "scipy >= 1.1.0",
     "matplotlib >= 1.5",
-    #"seaborn >= 0.10.1",
+    "seaborn >= 0.11.0",
 ]
 
 setuptools.setup(name='hypnospy',
-        version='0.0.1',
+        version='0.0.3',
         author='Ignacio Pozuelo, Joao Palotti',
         author_email='ignacio_perez_pozuelo@alumni.brown.edu, joaopalotti@gmail.com',
         license='BSD',
         install_requires=requirements,
         packages=setuptools.find_packages(),
         #package_dir = {'': '.'},
-        url='',
+        url='https://github.com/ippozuelo/HypnosPy/',
         description='A Device-Agnostic, Open-Source Python Software for Wearable Circadian Rhythm and Sleep Analysis and Visualization',
-        long_description=open('README.md').read()
+        long_description_content_type="text/markdown",
+        long_description=open('README.md').read(),
+        python_requires='>=3.6',
 )
 
