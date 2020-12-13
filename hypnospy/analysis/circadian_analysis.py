5edc913558b935b3f0dd93bbe538d1b9bffc46e2
Joao Palotti <joaopalotti@gmail.com>
2020-12-13 16:23:13 +0300

    Modified circadian analysis module to follow the same structure as the rest of the package

--- hypnospy/analysis/circadian_analysis.py
+++ hypnospy/analysis/circadian_analysis.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np
 import pandas as pd
 import math
@@ -8,7 +10,7 @@ from scipy import linalg  # linear algebra (matrix) processing package
 from tqdm import tqdm, trange
 from collections import defaultdict
 from tensorflow.keras.preprocessing import timeseries_dataset_from_array
-from CosinorPy import file_parser, cosinor, cosinor1
+from CosinorPy import cosinor
 
 
 class CircadianAnalysis(object):
@@ -23,58 +25,66 @@ class CircadianAnalysis(object):
         """
         sets ssa results in w.ssa
         """
+        # TODO: this is extremely slow as it is now. At least we should try to do it with multithreads.
         print('=== Running SSA ===')
-        for idx, w in tqdm(enumerate(self.wearables)):
-            # print(wearable.get_pid())
-
-            if w.data.shape[0] == 0:
+        ssas = []
+        for idx, wearable in tqdm(enumerate(self.wearables)):
+            if wearable.data.shape[0] == 0:
                 warnings.warn("No data for PID %s. Skipping it." % wearable.get_pid())
                 continue
-
             print(idx)
-            w = self._get_SSA(w)
-            self.wearables[idx] = w
+            ssas.append(self._get_SSA(wearable))
+        return ssas
 
     # Extract SSA parameters, partial variances stored in ssa_io_pv, other stored in dict ssa, read from files in folder for
     # convenience, as full analysis takes a long time for each subject
-    def _get_SSA(self, w, cols=['hyp_act_x'], freqs=['15T']):
+    def _get_SSA(self, w, col='hyp_act_x', freq='15T'):
         ssa = defaultdict(dict)
-        for col in cols:
-            df = w.data[[w.get_experiment_day_col(), 'hyp_time_col', 'hyp_act_x']].resample('1T', on='hyp_time_col').mean()
-            df = df['hyp_act_x']
-            ssa[col]['r'], ssa[col]['pv'], ssa[col]['gk'], ssa[col]['wm'] = self._get_SSA_par(df, L=1440)
-            ssa[col]['df'] = df
+        df = w.data[[w.get_experiment_day_col(), 'hyp_time_col', col]].resample('1T', on='hyp_time_col').mean()[col]
 
-            pd.DataFrame(ssa[col]['gk'][:10, :]).to_csv('ssa_gk_' + 'self.filename' + col + '.csv', index=False)
-            pd.DataFrame(ssa[col]['wm']).to_csv('ssa_wm_' + 'self.filename' + col + '.csv', index=False)
-            pd.DataFrame(ssa[col]['pv']).to_csv('ssa_pv_' + 'self.filename' + col + '.csv', index=False)
-            for freq in freqs:
-                df_1 = pd.DataFrame(np.transpose(ssa[col]['gk']))
+        ssa['r'], ssa['pv'], ssa['gk'], ssa['wm'] = self._get_SSA_par(df, L=1440)
+        ssa['df'] = df
 
-                # if self.data freq is different than df.resample, then the below line will cause an error
-                # df_1 = df_1.set_index(self.data.index)
-                df = w.data[[w.get_experiment_day_col(), 'hyp_time_col', 'hyp_act_x']].resample('1T', on='hyp_time_col').mean()
-                df = df['hyp_act_x']
-                ssa[col]['df'] = df
+        # Joao says: not really sure why we should save these files:
+        #pd.DataFrame(ssa[col]['gk'][:10, :]).to_csv('ssa_gk_' + self.filename + col + '.csv', index=False)
+        #pd.DataFrame(ssa[col]['wm']).to_csv('ssa_wm_' + self.filename + col + '.csv', index=False)
+        #pd.DataFrame(ssa[col]['pv']).to_csv('ssa_pv_' + self.filename + col + '.csv', index=False)
 
-                # df_1 = df_1.set_index(df.index)
-                df_1 = df_1.set_index(ssa[col]['df'].index)
+        df_1 = pd.DataFrame(np.transpose(ssa['gk']))
 
-                df_2 = df_1.between_time('06:00', '23:00', include_start=True, include_end=True)
-                df_gk = df_2[0] + df_2[1]
-                # Get SSA acrophases
-                ssa[col]['acrophase'] = df_gk.resample('24H').agg(lambda x: np.nan if x.count() == 0 else x.idxmax())
-                # Get vectors of coarse-grain params for ML
-                ssa[col]['gksum' + freq] = df_gk.resample(freq).mean()
-                # Get trend i.e. mesor
-                ssa[col]['trend'] = df_1[0].resample('24H').mean()
-                # Get period in minutes
-                ssa[col]['period'] = (ssa[col]['acrophase'] - ssa[col]['acrophase'].shift(1)).astype('timedelta64[m]')
-        w.ssa = ssa
-        return w
+        # if self.data freq is different than df.resample, then the below line will cause an error
+        # df_1 = df_1.set_index(self.data.index)
+        df = w.data[[w.get_experiment_day_col(), 'hyp_time_col', col]].resample('1T', on='hyp_time_col').mean()[col]
+        ssa['df'] = df
+
+        # df_1 = df_1.set_index(df.index)
+        df_1 = df_1.set_index(ssa['df'].index)
+
+        df_2 = df_1.between_time('06:00', '23:00', include_start=True, include_end=True)
+        df_gk = df_2[0] + df_2[1]
+
+        # Get SSA acrophases
+        ssa['acrophase'] = df_gk.resample('24H').agg(lambda x: np.nan if x.count() == 0 else x.idxmax())
+        ssa['acrophase'].name = "acrophase"
+
+        # Get vectors of coarse-grain params for ML
+        ssa['gksum' + freq] = df_gk.resample(freq).mean()
+
+        # Get trend i.e. mesor
+        ssa['trend'] = df_1[0].resample('24H').mean()
+        ssa['trend'].name = "trend"
+
+        # Get period in minutes
+        ssa['period'] = (ssa['acrophase'] - ssa['acrophase'].shift(1)).astype('timedelta64[m]')
+        ssa["pid"] = w.get_pid()
+
+        # Merge results
+        results = pd.concat([ssa["acrophase"], ssa["trend"]], axis=1)
+
+        return results, ssa
 
     @staticmethod
-    def _get_SSA_par(df, L=1440):  # 2 <= L <= N/2
+    def _get_SSA_par(df, L=1440, n_max_tries=3):  # 2 <= L <= N/2
         N = len(df)
         K = N - L + 1
 
@@ -90,20 +100,19 @@ class CircadianAnalysis(object):
         X = list(dataset.as_numpy_iterator())[0]
         print(X.shape)
 
-        try:
-            U, s, V = linalg.svd(X,
-                                 full_matrices=True,
-                                 compute_uv=True,
-                                 overwrite_a=False,
-                                 check_finite=True,
-                                 lapack_driver='gesvd')
-        except:
-            U, s, V = linalg.svd(X,
-                                 full_matrices=True,
-                                 compute_uv=True,
-                                 overwrite_a=False,
-                                 check_finite=True,
-                                 lapack_driver='gesvd')
+        for t in range(n_max_tries):
+            try:
+                U, s, V = linalg.svd(X,
+                                     full_matrices=True,
+                                     compute_uv=True,
+                                     overwrite_a=False,
+                                     check_finite=True,
+                                     lapack_driver='gesvd')
+            except:
+                continue
+
+        if t == n_max_tries:
+            raise ValueError("SSA reached the max number of tries with error.")
 
         l = s ** 2  # partial variances
         r = len(s)  # np.linalg.matrix_rank(X) # matrix rank and total number of components
@@ -147,22 +156,23 @@ class CircadianAnalysis(object):
         wMatrix = np.array(wMatrix)
         return (r, l, gkList, wMatrix);
 
-    def run_cosinor(self):
+    def run_cosinor(self, col='hyp_act_x'):
         """
-        sets cosinor results in w.cosinor
+        returns a list with cosinor results
         """
+        cosinors = []
+        for idx, wearable in tqdm(enumerate(self.wearables)):
 
-        for idx, w in tqdm(enumerate(self.wearables)):
-
-            if w.data.shape[0] == 0:
+            if wearable.data.shape[0] == 0:
                 warnings.warn("No data for PID %s. Skipping it." % wearable.get_pid())
                 continue
 
-            print(idx)
-            w = self._get_cosinor(w)
-            self.wearables[idx] = w
+            cosinors.append(self._get_cosinor(wearable, col))
+        cosinors = pd.concat(cosinors)
+        # Returns a dataframe indexed by ["pid", w.experiment_day]
+        return cosinors.reset_index().set_index(["pid", cosinors.index.name])
 
-    def _get_cosinor(self, w, col='hyp_act_x', freqs=2):
+    def _get_cosinor(self, w, col):
     
         freq = w.get_frequency_in_secs()
         s = w.data.groupby(w.get_experiment_day_col())['hyp_time_col'].transform(self._group_to_timepoints, freq)
@@ -171,43 +181,34 @@ class CircadianAnalysis(object):
         cosinor_input = w.data[[w.get_experiment_day_col(), 'cosinor_timepoints', col]].copy()
         cosinor_input['cosinor_timepoints'] = s
         cosinor_input = cosinor_input[[w.get_experiment_day_col(), 'cosinor_timepoints', col]]
-        cosinor_input.columns = ['test', 'x', 'y']
-        cosinor_input['test'] = cosinor_input['test'].astype(str)
+        cosinor_input.columns = [w.get_experiment_day_col(), 'x', 'y']
 
-        cosinor_result = cosinor_input.groupby('test').apply(self._apply_cosinor_on_exp_day)
+        cosinor_result = cosinor_input.groupby(w.get_experiment_day_col()).apply(self._apply_cosinor_on_exp_day)
         cosinor_result.index = cosinor_result.index.astype(int)
 
         # remove invalid days
-        cosinor_result = cosinor_result.loc[0:]
-        
+        # cosinor_result = cosinor_result.loc[0:]
+
+        cosinor_result["pid"] = w.get_pid()
 
         # cosinor_result is a tuple of
-        # (RegressionResultsWrapper, statistics, rhythm_params, X_test, Y_test, model) 
-        w.cosinor = cosinor_result
-        return w
+        # (pid, RegressionResultsWrapper, statistics, rhythm_params, X_test, Y_test, model)
+        return cosinor_result
 
     @staticmethod
-    def _apply_cosinor_on_exp_day(g):
-        try:
-            cosinor_result = cosinor.fit_me(g['x'].values,
-                       g['y'].values, 
-                       n_components=2, 
-                       period=24, 
-                       model_type='lin', 
-                       lin_comp=True, 
-                       alpha=0,
-                       plot=False)
-        except:
-            cosinor_result = cosinor.fit_me(g['x'].values,
-                       g['y'].values, 
-                       n_components=2, 
-                       period=24, 
-                       model_type='lin', 
-                       lin_comp=True, 
-                       alpha=0,
-                       plot=False)
+    def _apply_cosinor_on_exp_day(g, n_max_tries=3):
+        for t in range(n_max_tries):
+            try:
+                cosinor_result = cosinor.fit_me(g['x'].values, g['y'].values, n_components=2, period=24,
+                                                model_type='lin', lin_comp=True, alpha=0, plot=False)
+            except:
+                continue
+
+        if t == n_max_tries:
+            raise ValueError("SSA reached the max number of tries with error.")
+
         # cosinor_result[1] contains {p, p_reject, SNR, RSS, resid_SE, ME}
-        # cosinor_result[2] contains {ME, period, aplitude, acrophase, mesor}
+        # cosinor_result[2] contains {ME, period, amplitude, acrophase, mesor}
         # Note: sometimes, acrophase is NaN.
         df = pd.concat([pd.Series(cosinor_result[1]), pd.Series(cosinor_result[2])], axis=0)
         return df
@@ -220,7 +221,6 @@ class CircadianAnalysis(object):
 
     def run_PSD():
         # apply power spectral density analysis
-        df['PSD'] = PSD
         pass
 
     def run_entropy(df, bins=20):
