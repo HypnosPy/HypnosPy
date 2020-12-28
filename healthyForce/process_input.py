873e8a13502b4a69b30b5c9d28cb5db80c598f72
Joao Palotti <joaopalotti@gmail.com>
2020-12-28 17:54:57 +0300

    Updated current process_input

--- healthyForce/process_input.py
+++ healthyForce/process_input.py
@@ -1,4 +1,7 @@
+# 27 dec
+import time
 import functools
+import sys
 import pandas as pd
 import numpy as np
 from glob import glob
@@ -33,11 +36,16 @@ def setup_experiment(preprocessor, file_path, start_hour):
 
     return exp
 
+def check_time(t):
+    now = time.process_time()
+    print("Elapsed %.1f sec (=%.2f min):" % (now - t, (now - t)/60))
+    return now
+
 
 if __name__ == "__main__":
 
     # options are: HCHS_SMALL, MESA_SMALL, HCHS, MESA
-    DATA_COLLECTION = "HCHS"  #
+    DATA_COLLECTION = "HCHS_SMALL" # sys.argv[1]
 
     if DATA_COLLECTION == "MESA_SMALL":
         file_path = "../data/small_collection_mesa/*.csv"
@@ -96,16 +104,24 @@ if __name__ == "__main__":
     start_hour = 15
     min_consecutive_days = 5
 
+    print("Reading collection from disk...")
+    start = time.process_time()
+
     exp = setup_experiment(preprocessor, file_path, start_hour)
     exp.fill_no_activity(-0.0001)
     exp.overall_stats()
 
+    elapsed_time = check_time(start)
+
     # TODO: fix bug when annotation_merge_tolerance_in_minutes < 0
+    print("Sleep Boundary Detector...")
     sbd = SleepBoudaryDetector(exp)
     sbd.detect_sleep_boundaries(strategy="annotation", output_col="sleep_period_annotation",
                                 annotation_col="interval_sleep",
                                 annotation_merge_tolerance_in_minutes=120, annotation_only_largest_sleep_period=True)
 
+    elapsed_time = check_time(elapsed_time)
+    print("Validation process...")
     va = Validator(exp)
     va.flag_epoch_physical_activity_less_than(min_activity_threshold=0)
     va.flag_epoch_null_cols(col_list=["hyp_act_x"])
@@ -131,6 +147,8 @@ if __name__ == "__main__":
     # n_removed_days = va.remove_flagged_days()
     # print("Removed %d days (few valid epochs)." % n_removed_days)
 
+    elapsed_time = check_time(elapsed_time)
+    print("Done flagging wearables")
     va.validation_report()
 
     n_removed_wearables = va.remove_wearables_without_valid_days()
@@ -142,14 +160,29 @@ if __name__ == "__main__":
     n_removed_wearables = va.remove_wearables_without_valid_days()
     print("Removed %d wearables." % n_removed_wearables)
 
+    elapsed_time = check_time(elapsed_time)
+    print("Done removing wearables")
+
     exp.overall_stats()
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done showing overall stats")
 
     # Setting day to ml representation -> days may not be of fixed lengths.
     print("Changing experiment_day representation to ml_sequence")
     exp_day_column = 'ml_sequence'
     exp.create_day_sleep_experiment_day(sleep_col="sleep_period_annotation", new_col=exp_day_column)
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done with creating the ml_sequence col")
+
     va.flag_day_sleep_length_less_than(sleep_period_col="sleep_period_annotation", min_sleep_in_minutes=3 * 30)
     va.flag_day_sleep_length_more_than(sleep_period_col="sleep_period_annotation", max_sleep_in_minutes=12 * 60)
+    va.flag_day_if_valid_epochs_smaller_than(valid_minutes_per_day=20 * 60)
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done with flagging new sequences")
+
     n_removed_days = va.remove_flagged_days()
     print("Removed additional %d invalid days." % n_removed_days)
 
@@ -160,8 +193,14 @@ if __name__ == "__main__":
     n_removed_wearables = va.remove_wearables_without_valid_days()
     print("Removed %d wearables." % n_removed_wearables)
 
+    elapsed_time = check_time(elapsed_time)
+    print("Done removing wearables")
+
     print("FINAL DATASET:")
     exp.overall_stats()
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done dataset preprocessing...")
 
     print("Calculating PA features")
     pa_levels = ["sedentary", "light", "medium", "vigorous"]
@@ -187,19 +226,22 @@ if __name__ == "__main__":
         lambda left, right: pd.merge(left, right, on=["pid", exp_day_column, "hyp_time_col", "bout_length"],
                                      how='outer'), bouts).fillna(0.0)
 
-    bouts_melted = bouts.melt(id_vars=["pid", exp_day_column, "bout_length"],
-                              value_vars=value_vars)
+    # bouts_melted = bouts.melt(id_vars=["pid", exp_day_column, "bout_length"],
+    #                           value_vars=value_vars)
 
     bouts['hyp_time_col'] = pd.Categorical(bouts['hyp_time_col'], ordered=True, categories=range(24))
     # Make sure that this wont happen
     bouts = bouts[bouts[exp_day_column] != -1]
 
+    # TODO: bouts_per_hour need to be saved as well
     bouts_per_hour = bouts.groupby(['pid', exp_day_column, 'hyp_time_col', 'bout_length'])[value_vars].mean().fillna(0)
 
-    bouts_per_day = bouts_per_hour.groupby(['pid', exp_day_column, 'bout_length']).mean()
+    bouts_per_day = bouts_per_hour.groupby(['pid', exp_day_column, 'bout_length']).sum() # Total number of bouts per day
     bouts_per_day = bouts_per_day.pivot_table(index=['pid', exp_day_column],
                                               columns=['bout_length'],
                                               values=value_vars)
+    # Flatten hierarchical column names
+    bouts_per_day.columns = ['_'.join(map(str, col)).strip() for col in bouts_per_day.columns.values]
 
     pa_bins = pa.get_binned_pa_representation()
     pa_bins_per_hour = pa_bins.pivot_table(index=['pid', exp_day_column, "hyp_time_col"])
@@ -211,17 +253,29 @@ if __name__ == "__main__":
 
     pa_raw_per_hour = pa.get_raw_pa("hour")
     pa_raw_per_hour = pa_raw_per_hour.set_index(['pid', exp_day_column, "hyp_time_col"])
+    #pa_raw_per_hour["raw_pa"] = pa_raw_per_hour["raw_pa"].apply(lambda x: ','.join(x.astype(str)))
+
     pa_raw_per_day = pa.get_raw_pa("day")
     pa_raw_per_day = pa_raw_per_day.set_index(['pid', exp_day_column])
+    pa_raw_per_day["raw_pa"] = pa_raw_per_day["raw_pa"].apply(lambda x: ','.join(x.astype(str)))
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done with PA features...")
 
     # Sleep Analysis
     print("Calculating Sleep Metrics")
     sm = SleepMetrics(exp)
-    sleep_metrics = ["sleepEfficiency", "awakening", "totalSleepTime"]
+    sleep_metrics = ["sleepEfficiency", "totalSleepTime"]
     sleep_metrics_per_day = []
     for sleep_metric in sleep_metrics:
         sleep_metrics_per_day.append(sm.get_sleep_quality(sleep_metric=sleep_metric, wake_sleep_col="sleep",
                                                           sleep_period_col="sleep_period_annotation"))
+
+    sleep_metrics.append("awakening")
+    sleep_metrics_per_day.append(sm.get_sleep_quality(sleep_metric="awakening", wake_sleep_col="sleep",
+                                                      ignore_awakenings_smaller_than_X_minutes=5,
+                                                      sleep_period_col="sleep_period_annotation"))
+
     # SRI calculation will not work with set_ml_representation_days_exp because day representation will be of different lengths.
     # While SRI requires the days to be of fixed lengths.
     # sleep_metrics.append("sri")
@@ -232,6 +286,9 @@ if __name__ == "__main__":
         sleep_metrics_per_day)
     sleep_metrics_per_day = sleep_metrics_per_day.set_index(['pid', 'ml_sequence'])[sleep_metrics]
 
+    elapsed_time = check_time(elapsed_time)
+    print("Done with sleep metrics...")
+
     # Circadian Analysis
     print("Running Circadian Analysis")
     ca = CircadianAnalysis(exp)
@@ -239,6 +296,9 @@ if __name__ == "__main__":
     cosinor_keys = ["amplitude", "acrophase", "mesor"]
     cosinor_per_day = ca.run_cosinor()
     cosinor_per_day = cosinor_per_day[cosinor_keys]
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done with circadian features...")
 
     # SSA is not yet ready to be used.
     # ssa = ca.run_SSA()
@@ -249,11 +309,18 @@ if __name__ == "__main__":
     for col in demo.data.keys():
         demo.data[col] = demo.data[col].astype(np.float)
 
+    elapsed_time = check_time(elapsed_time)
+    print("Done with demographic features...")
+
     # Time related features
     print("Calculating Time related features")
     weekend = exp.is_weekend()
     weekday = exp.weekday()
     time_features_per_day = pd.merge(weekend, weekday).set_index(["pid", exp_day_column])
+
+    elapsed_time = check_time(elapsed_time)
+    print("Done with time features...")
+
 
     day_keys = {"bins": list(pa_bins_per_day.keys()),
                "bouts": list(bouts_per_day.keys()),
@@ -273,8 +340,12 @@ if __name__ == "__main__":
         time_features_per_day, left_index=True, right_index=True
     )
 
-    data_representation_per_hour = pa_raw_per_hour
-    data_representation_per_pid = demo.data
+    data_representation_per_hour = pd.merge(bouts_per_hour, pa_bins_per_hour, left_index=True, right_index=True,
+                                            suffixes=("_bouts", "_bins")).merge(
+        pa_stats_per_hour, left_index=True, right_index=True).merge(
+        pa_raw_per_hour, left_index=True, right_index=True
+    )
+    data_representation_per_pid = demo.get_valid_data_for_experiment(exp)
 
     data_representation_per_day.to_csv("%s_per_day.csv" % DATA_COLLECTION)
     data_representation_per_hour.to_csv("%s_per_hour.csv" % DATA_COLLECTION)
@@ -283,6 +354,11 @@ if __name__ == "__main__":
     day_keys.index.name = "key"
     day_keys.name = "value"
     day_keys.to_csv("%s_day_keys.csv" % DATA_COLLECTION)
+
+    pd.Series(data_representation_per_day.reset_index()["pid"].unique(), name="pids").to_csv("%s_pid.csv" % DATA_COLLECTION, index=False)
+
+    check_time(start)
+    print("Done with all...")
 
     # View signals
     # v = Viewer(exp)
